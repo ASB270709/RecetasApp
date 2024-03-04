30f6039806abdd869318fa38c943b560d5faf974
ASB270709 <130920728+ASB270709@users.noreply.github.com>
2024-03-03 22:46:26 -0600

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,37 +1,67 @@
+
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, FlatList } from 'react-native';
+import axios from 'axios';
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [recipes, setRecipes] = useState([]);
+  const [term, setTerm] = useState('');
+  const [recipes] = useState([]);
+  const [selectedRecipe] = useState(null);
 
-  const API_KEY = '9c7b888b98e74dc583a5c83d8fd1b65b'
+  const API_KEY = '9c7b888b98e74dc583a5c83d8fd1b65b'; 
 
   const searchRecipes = async () => {
-    const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${searchTerm}&apiKey=${API_KEY}`);
-    const data = await response.json();
-    setRecipes(data.results);
+    try {
+      const response = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&query=${term}`);
+      //Para comprobar las recetas recibidas atraves de la consola
+      /*console.log('Response Data:', response.data); 
+      setRecipes(response.data.results);*/
+    } catch (error) {
+      console.error('Error:', error); 
+    }
+  };
+
+  const getRecipeDetails = async (id) => {
+    try {
+      const response = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY}`);
+     //Para comprobar los datos que llegan de la consulta
+     /* console.log('Detalles:', response.data); 
+      setSelectedRecipe(response.data);*/
+    } catch (error) {
+      console.error('Error:', error); 
+    }
   };
   
-
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <TextInput
-        value={searchTerm}
-        onChangeText={setSearchTerm}
-        placeholder="Buscar recetas"
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginTop: 50, backgroundColor: 'gray' }}
+        value={term}
+        onChangeText={setTerm}
+        placeholder="Buscar recetas (Solo en Inglés)"
       />
-      <Button title="Buscar" onPress={searchRecipes} />
+      <Button
+        title="Buscar"
+        onPress={searchRecipes}
+      />
       <FlatList
         data={recipes}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
-          <View>
-            <Text>{item.title}</Text>
-            {}
+          <View style={{ padding: 10, borderBottomColor: '#ccc', borderBottomWidth: 1 }}>
+            <Text style={{ fontSize: 18 }} onPress={() => getRecipeDetails(item.id)}>{item.title}</Text>
           </View>
         )}
       />
+      {selectedRecipe && (
+        <View>
+          <Text style={{ fontSize: 20, fontWeight: 'bold', borderColor:'gray', borderWidth: 1 }}>{selectedRecipe.title}</Text>
+          <Text style={{ fontSize: 16, borderColor:'gray', borderWidth: 1 }}>Ingredientes:</Text>
+          {selectedRecipe.extendedIngredients.map((ingredient, index) => (
+            <Text key={index}>{ingredient.original}</Text>
+          ))}
+        </View>
+      )}
     </View>
   );
 };
